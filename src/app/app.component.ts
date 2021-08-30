8f28b5f17b95078f0bb32263ddc87756e5d16fbc
Amoyensis <amoyensis@outlook.com>
2021-08-30 21:29:12 +0800

    蜂巢

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { AuthService } from './auth/auth.service';
+
+function getStyle(obj: any, name: any) {
+  if (obj.currentStyle) {
+    return obj.currentStyle[name];
+  } else {
+    return getComputedStyle(obj, null)[name];
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -8,39 +14,36 @@ import { AuthService } from './auth/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'WebClient';
-  items = [
-    {
-        label: 'File',
-        items: [{
-                label: 'New',
-                icon: 'pi pi-fw pi-plus',
-                items: [
-                    {label: 'Project'},
-                    {label: 'Other'},
-                ]
-            },
-            {label: 'Open'},
-            {label: 'Quit'}
-        ]
-    },
-    {
-        label: 'Edit',
-        icon: 'pi pi-fw pi-pencil',
-        items: [
-            {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-            {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-        ]
-    }
-  ];
-
-  constructor(private http: HttpClient, private auth: AuthService) {}
+  data: any[] = [];
 
   ngOnInit() {
-    this.auth.login('amoyensis@outlook.com', '123456');
-    // 是不是以斜杠起头无所谓，但是一定要以api/起头
-    this.http.get('/api/user').subscribe(res => {
-      console.log(res);
+    this.event();
+    let i = 0;
+    while (i < 18) {
+      this.data.push(Math.random());
+      i++;
+    }
+  }
+
+  event() {
+    let div = document.querySelector('section') as unknown as HTMLElement;
+    let width: number, height: number;
+    let x: number, y: number;
+    window.addEventListener(
+      'mousemove',
+      (ev) => {
+        x = ev.clientX - width / 2;
+        y = ev.clientY - height / 2;
+      },
+      false
+    );
+    setInterval(() => {
+      width = document.documentElement.clientWidth;
+      height = document.documentElement.clientHeight;
+      let left = parseInt(getStyle(div, 'left'));
+      let top = parseInt(getStyle(div, 'top'));
+      div && ( div.style.left = left + ( - x / 5 - left ) * 0.05 + 'px' );
+      div && ( div.style.top = top + ( - y / 5 - top ) * 0.05 + 'px' )
     })
   }
 }
